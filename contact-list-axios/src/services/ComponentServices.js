6f768ef330e87f814e916511233a188bb9c421c6
Noelia Qualindi <noelia.qualindi@web.com>
2019-10-08 18:52:13 -0300

    Deleted old project

--- contact-list-axios/src/services/ComponentServices.js
+++
@@ -1,9 +0,0 @@
-import AxiosInstance, { setHeaders } from './serviceConfig';
-
-const ComponentServices = {
-    getContacts: () => {
-          return AxiosInstance.get('/v3/contacts.json');
-    }
-}
-
-export default ComponentServices;
